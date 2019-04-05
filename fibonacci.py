14dd93222053803b7e4143caef05d6ffb0a5c365
Alex C. Viana <alexcostaviana@gmail.com>
2019-04-04 20:23:42 -0500

    Updated the fibonacci module to start at 0 not 1.

--- fibonacci.py
+++ fibonacci.py
@@ -5,30 +5,38 @@ A function that generates the Fibonacci sequence through the nth term.
 '''
 
 def fibonacci_number(n):
+    '''
+    A naive recursive implementation that returns the nth Fibonacci
+    number.
+    '''
     if n == 1:
-        return 1
+        return 0
     elif n == 2:
-        return 2
+        return 1
     else:
         return fibonacci_number(n - 1) + fibonacci_number(n - 2)
 
 
 def test_fibonacci_number():
-    assert fibonacci_number(5) == 8, fibonacci_number(5)
+    assert fibonacci_number(7) == 8, fibonacci_number(8)
 
 
 def fibonacci_sequence(n):
+    '''
+    A naive implementation that returns 1st n values in the Fibonacci
+    series.
+    '''
     if n == 1:
-        return [1]
+        return [0]
     elif n == 2:
-        return [1, 2]
+        return [0, 1]
     else:
         temp = fibonacci_sequence(n - 1)
         return temp + [temp[-1] + temp[-2]]
 
 
 def test_fibonacci_sequence():
-    assert fibonacci_sequence(5) == [1, 2, 3, 5, 8], fibonacci_sequence(5)
+    assert fibonacci_sequence(7) == [0, 1, 1, 2, 3, 5, 8], fibonacci_sequence(7)
 
 
 if __name__ == '__main__':
