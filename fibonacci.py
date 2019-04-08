841d05bd8957ac87c71d2f787437246ded579afc
Alex C. Viana <alexcostaviana@gmail.com>
2019-04-07 21:02:11 -0500

    Updated fibonacci to start at 0 instead of 1.

--- fibonacci.py
+++ fibonacci.py
@@ -1,6 +1,5 @@
 '''
 A function that generates the nth Fibonacci number.
-
 A function that generates the Fibonacci sequence through the nth term.
 '''
 
@@ -9,9 +8,9 @@ def fibonacci_number(n):
     A naive recursive implementation that returns the nth Fibonacci
     number.
     '''
-    if n == 1:
+    if n == 0:
         return 0
-    elif n == 2:
+    elif n == 1:
         return 1
     else:
         return fibonacci_number(n - 1) + fibonacci_number(n - 2)
@@ -26,9 +25,9 @@ def fibonacci_sequence(n):
     A naive implementation that returns 1st n values in the Fibonacci
     series.
     '''
-    if n == 1:
+    if n == 0:
         return [0]
-    elif n == 2:
+    elif n == 1:
         return [0, 1]
     else:
         temp = fibonacci_sequence(n - 1)
@@ -40,5 +39,14 @@ def test_fibonacci_sequence():
 
 
 if __name__ == '__main__':
-    test_fibonacci_number()
-    test_fibonacci_sequence()
+    # test_fibonacci_number()
+    # test_fibonacci_sequence()
+
+    # import timeit
+    # for n in range(0, 51, 5):
+    #     trial = timeit.timeit(f'fibonacci_sequence(n)', globals=globals())
+    #     print(f'N = {n}: {trial} s')
+
+
+    # import cProfile
+    # cProfile.run('fibonacci_sequence(5)')
